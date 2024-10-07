a1ae864fced36765d1b783ae052b35b7f9bef507
KathereneLugo <klugo@macalester.edu>
2024-10-07 10:51:06 -0500

    Exercise finished

--- src/cellabsorption/CellSimulation.java
+++ src/cellabsorption/CellSimulation.java
@@ -12,15 +12,9 @@ import java.util.Random;
 @SuppressWarnings("SameParameterValue")
 public class CellSimulation {
     private List<Cell> cells;
-    // private static final double
-    //     WIGGLINESS = 0.2,
-    //     WANDER_FROM_CENTER = 60000;
 
     private CanvasWindow canvas;
     private Random rand = new Random();
-    // private Ellipse shape;
-    // private double radius;
-    // private double direction;
 
     public static void main(String[] args) {
         new CellSimulation();
@@ -33,10 +27,9 @@ public class CellSimulation {
         //noinspection InfiniteLoopStatement
         while (true) {
             Point canvasCenter = new Point(canvas.getWidth() / 2.0, canvas.getHeight() / 2.0);
-            // Cell cell = new Cell(3, 3, 2, Color.PINK);
+            handleCellInteraction();
             for( Cell c: cells) {
                 c.moveAround(canvasCenter);
-                c.grow(0.02);
                 }
             canvas.draw();
             canvas.pause(10);
@@ -57,46 +50,15 @@ public class CellSimulation {
         }
     }
 
-    // private void createCell(double x, double y, double radius, Color color) {
-    //     shape = new Ellipse(x, y, radius * 2, radius * 2);
-    //     shape.setFillColor(color);
-    //     this.radius = radius;
-    //     direction = normalizeRadians(Math.random() * Math.PI * 2);
-    // }
-
-    // private void grow(double amount) {
-    //     setRadius(radius + amount);
-    // }
-
-    // private void setRadius(double newRadius) {
-    //     if (newRadius < 0) {
-    //         newRadius = 0;
-    //     }
-    //     radius = newRadius;
-    //     Point previousCenter = shape.getCenter();
-    //     shape.setSize(new Point(newRadius * 2, newRadius * 2));
-    //     shape.setCenter(previousCenter);
-    // }
-
-    // private void moveAround(Point centerOfGravity) {
-    //     shape.moveBy(Math.cos(direction), Math.sin(direction));
-
-    //     double distToCenter = shape.getCenter().distance(centerOfGravity);
-    //     double angleToCenter = centerOfGravity.subtract(shape.getCenter()).angle();
-    //     double turnTowardCenter = normalizeRadians(angleToCenter - direction);
-
-    //     direction = normalizeRadians(
-    //         direction
-    //             + (Math.random() - 0.5) * WIGGLINESS
-    //             + turnTowardCenter * Math.tanh(distToCenter / WANDER_FROM_CENTER));
-    // }
-
-    private static double sqr(double x) {
-        return x * x;
+    private void handleCellInteraction() {
+        for (int i = 0; i < cells.size(); i++) {
+            Cell cell = cells.get(i);
+            for (int j = i + 1; j < cells.size(); j++) {
+                Cell cell0 = cells.get(j);
+                cell.interactWith(cell0);    
+            }
+            
+        }
     }
 
-    // private static double normalizeRadians(double theta) {
-    //     double pi2 = Math.PI * 2;
-    //     return ((theta + Math.PI) % pi2 + pi2) % pi2 - Math.PI;
-    // }
 }
